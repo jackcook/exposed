19565add18440fd9effdaaa74bf520af0e8c7e55
Jack Cook <hello@jackcook.nyc>
2016-08-07 08:46:41 -0400

    Made things pretty

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function update_tab(name) {
+function update_tab(elem, name) {
+  var cells = document.getElementsByClassName("individual");
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].className = "individual";
+  }
+
+  elem.className = "individual selected";
+
   iframe = document.getElementsByTagName("iframe")[0];
   iframe.src = "tab.html?name=" + name;
 }
